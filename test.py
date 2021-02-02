b93d5a2948bdadb94c55a224f82a9c087d226d9f
CobbCoding1 <rircobb@gmail.com>
2021-02-02 17:10:15 -0500

    Added enemy with basic movement

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -13,8 +14,17 @@ playerX = 608
 playerY = 650
 playerX_change = 0
 
+enemyImg = pygame.image.load('space-ship.png')
+enemyX = random.randint(0, 1248)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.5
+enemyY_change = 40
+
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 running = True
 
@@ -42,5 +52,15 @@ while running:
     elif(playerX >= 1248):
         playerX = 1248
 
+    enemyX += enemyX_change
+
+    if(enemyX <= 0):
+        enemyX_change = 0.3
+        enemyY += enemyY_change
+    elif(enemyX >= 1248):
+        enemyX_change = -0.3
+        enemyY += enemyY_change
+
     player(playerX, playerY)
+    enemy(enemyX, enemyY)
     pygame.display.update()
